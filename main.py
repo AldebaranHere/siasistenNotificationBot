b1588dd2b7c065bcd082244d6404ce30af1ed4e6
Aldebaran64 <aldebaran26adhitya@gmail.com>
2026-01-01 11:53:21 +0700

    Edited bot to adapt depending on OS

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pickle
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import platform
 
 load_dotenv()
 
@@ -21,12 +22,6 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
-
-# PythonAnywhere specific: Tell it where the browser is
-chrome_options.binary_location = "/usr/bin/chromium-browser"
-
-# Use the pre-installed driver path
-service = Service(executable_path="/usr/bin/chromedriver")
 
 class Client(commands.Bot):
     async def on_ready(self):
@@ -57,72 +52,82 @@ async def background_loop():
         await channel.send(f"=================================================================")
         await channel.send(f"Pengecekan dimulai! Simak informasi berikut:")
 
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        if platform.system() == "Linux":
+            # Use case: PythonAnywhere or a Linux server
+            chrome_options.binary_location = "/usr/bin/chromium-browser"
+            service = Service(executable_path="/usr/bin/chromedriver")
+            driver = webdriver.Chrome(service=service, options=chrome_options)
+        else:
+            # Automatic Windows/Mac detection
+            driver = webdriver.Chrome(options=chrome_options)
 
-        driver.get("https://siasisten.cs.ui.ac.id/lowongan/listLowongan/")
+        try:
+            driver.get("https://siasisten.cs.ui.ac.id/lowongan/listLowongan/")
 
-        with open("cookies.pkl", "rb") as file:
-            cookies = pickle.load(file)
+            with open("cookies.pkl", "rb") as file:
+                cookies = pickle.load(file)
 
-        for cookie in cookies:
-            if 'expiry' in cookie:
-                cookie['expiry'] = int(cookie['expiry'])
-            driver.add_cookie(cookie)
+            for cookie in cookies:
+                if 'expiry' in cookie:
+                    cookie['expiry'] = int(cookie['expiry'])
+                driver.add_cookie(cookie)
 
-        time.sleep(2)
-        driver.refresh()
+            time.sleep(2)
+            driver.refresh()
 
-        time.sleep(2)
-        driver.find_element(By.ID, "id_username").send_keys(SIASISTEN_USERNAME)
-        driver.find_element(By.ID, "id_password").send_keys(SIASISTEN_PASSWORD)
-        driver.find_element(By.CSS_SELECTOR, "input[value='login']").click()
+            time.sleep(2)
+            driver.find_element(By.ID, "id_username").send_keys(SIASISTEN_USERNAME)
+            driver.find_element(By.ID, "id_password").send_keys(SIASISTEN_PASSWORD)
+            driver.find_element(By.CSS_SELECTOR, "input[value='login']").click()
 
-        time.sleep(2)
-        driver.refresh()
+            time.sleep(2)
+            driver.refresh()
 
-        mata_kuliah = {
-            "SDA": "CSGE602040",
-            "MD2": "CSGE601011",
-            "Kalkulus2": "CSCM601213",
-            "POK": "CSCM601252",
-            "DDP2": "CSGE601021",
+            mata_kuliah = {
+                "SDA": "CSGE602040",
+                "MD2": "CSGE601011",
+                "Kalkulus2": "CSCM601213",
+                "POK": "CSCM601252",
+                "DDP2": "CSGE601021",
 
-            "Kalkulus1": "CSGE601012",
-            "DDP1": "CSGE601020",
-            "PSD": "CSCM601150",
-            "MD1": "CSGE601010",
-            "ALIN": "CSGE602012"
-        }
-        guild = channel.guild
-        role_data = {r.name: r.id for r in guild.roles[1:]}
+                "Kalkulus1": "CSGE601012",
+                "DDP1": "CSGE601020",
+                "PSD": "CSCM601150",
+                "MD1": "CSGE601010",
+                "ALIN": "CSGE602012"
+            }
+            guild = channel.guild
+            role_data = {r.name: r.id for r in guild.roles[1:]}
 
-        time.sleep(2)
-        # Find the table
-        # Syntax is verbose due to the HTML content of the webpage
-        table = driver.find_element(By.XPATH, "//h4[@id='next-term-header']/following-sibling::table[1]")
-        rows = table.find_elements(By.TAG_NAME, "tr")
+            time.sleep(2)
+            # Find the table
+            # Syntax is verbose due to the HTML content of the webpage
+            table = driver.find_element(By.XPATH, "//h4[@id='next-term-header']/following-sibling::table[1]")
+            rows = table.find_elements(By.TAG_NAME, "tr")
 
-        for mk_name, mk_code in mata_kuliah.items():
-            is_not_available = True
-            for row in rows:
-                cells = row.find_elements(By.TAG_NAME, "td")
-                
-                # Skip <th> header rows
-                if len(cells) > 0:
+            for mk_name, mk_code in mata_kuliah.items():
+                is_not_available = True
+                for row in rows:
+                    cells = row.find_elements(By.TAG_NAME, "td")
                     
-                    course_info = cells[1].text
-                    status = cells[3].text.strip()
+                    # Skip <th> header rows
+                    if len(cells) > 0:
+                        
+                        course_info = cells[1].text
+                        status = cells[3].text.strip()
 
-                    if mk_code in course_info and "Buka" in status:
-                        is_not_available = False
-                        await channel.send(f"<@&{role_data[mk_name]}> {mk_name} sudah dibuka! Segera daftar di https://siasisten.cs.ui.ac.id/lowongan/listLowongan/")
-            
-            if is_not_available:
-                await channel.send(f"<@&{role_data[mk_name]}> {mk_name} belum dibuka atau sudah penuh")
+                        if mk_code in course_info and "Buka" in status:
+                            is_not_available = False
+                            await channel.send(f"<@&{role_data[mk_name]}> {mk_name} sudah dibuka! Segera daftar di https://siasisten.cs.ui.ac.id/lowongan/listLowongan/")
+                
+                if is_not_available:
+                    await channel.send(f"<@&{role_data[mk_name]}> {mk_name} belum dibuka atau sudah penuh")
 
-        await channel.send(f"Pengecekan selesai! Tunggu 3 menit untuk pengecekan selanjutnya.")
-        await channel.send(f"=================================================================")
-        driver.quit()
+            await channel.send(f"Pengecekan selesai! Tunggu 3 menit untuk pengecekan selanjutnya.")
+            await channel.send(f"=================================================================")
+        
+        finally:
+            driver.quit()
 
     else:
         print(f"Channel with ID {CHANNEL_ID} not found.")
